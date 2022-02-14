1ec29b9d57fe9dfd6a663f38f30fae01fe19bc86
David Castro <campeador.david@gmail.com>
2022-02-14 15:27:04 -0700

    Configuracion Cors
    Muy importante configurar si no funciona

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express  from "express";
 import { graphqlHTTP } from "express-graphql";
 import { schema } from "./schema";
+const cors = require('cors')
 const  app = express()
 
-app.use('/graphql',graphqlHTTP(
+app.use('/graphql',cors(),graphqlHTTP(
     {
         graphiql:true,
         schema
